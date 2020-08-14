91220ad285f482d6cd47a67f54def6368d586601
Laroshan Surendran <laroshanlaro@gmail.com>
2020-08-14 22:09:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Home from "./pages/Home";
 import Room from "./pages/Room";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import {Route,Switch} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <>
-        <h4>hello</h4>
-        <Home/> 
-         <Room/>
-        <SingleRoom/>
-        <Error/> 
+        
+         <Switch>
+         <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms" component={Room}/>
+        <Route exact path="/rooms/:roomid" component={SingleRoom}/>
+        <Route component={Error}/>
+         </Switch>
+       
+        
+         
       </>
     )
   }
